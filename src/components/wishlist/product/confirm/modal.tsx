bc0bff336700ee584f173e3c52be37d4edb64ad2
Ethan Grebmeier <ethan@assembleinc.com>
2024-08-04 19:34:25 -0700

    feat: separate attribution process

--- src/components/wishlist/product/confirm/modal.tsx
+++ src/components/wishlist/product/confirm/modal.tsx
@@ -117,141 +117,163 @@ const ModalContent = ({
   product,
   wishlistShares,
 }: ModalContentProps) => {
+  const [frame, setFrame] = useState<"attribute" | "confirm">("attribute");
   const router = useRouter();
   const [selectedUserId, setSelectedUserId] = useState<string | undefined>();
   const { execute } = useAction(markProductReceived, {
     onSuccess: () => router.refresh(),
   });
 
-  return (
-    <form
-      action={() => {
-        execute({
-          productId: product.id,
-          wishlistId: wishlist.id,
-          purchasedByUserId: selectedUserId,
-        });
-      }}
-      className="relative  overflow-hidden pt-2"
-    >
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-col ">
-          <p className="mb-6 text-xl font-medium">
-            {" "}
-            Marking this item as received will move it{" "}
-          </p>
-          <div className="grid grid-cols-[1fr_auto_1fr] flex-row flex-nowrap items-center gap-2">
-            <div>
-              <p className="mb-2 text-xl font-medium "> From: </p>
-              <MockCard
-                title={wishlist.name}
-                color={getBackgroundColor(wishlist.color) ?? "bg-white"}
-              >
-                {wishlist.imageUrl ? (
-                  <img
-                    src={wishlist.imageUrl}
-                    className="w-full object-cover object-center"
-                  />
-                ) : (
+  if (frame === "confirm") {
+    return (
+      <form
+        action={() => {
+          execute({
+            productId: product.id,
+            wishlistId: wishlist.id,
+            purchasedByUserId: selectedUserId,
+          });
+        }}
+        className="relative  overflow-hidden pt-2"
+      >
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col ">
+            <p className="mb-6 text-xl font-medium">
+              {" "}
+              Marking this item as received will move it{" "}
+            </p>
+            <div className="grid grid-cols-[1fr_auto_1fr] flex-row flex-nowrap items-center gap-2">
+              <div>
+                <p className="mb-2 text-xl font-medium "> From: </p>
+                <MockCard
+                  title={wishlist.name}
+                  color={getBackgroundColor(wishlist.color) ?? "bg-white"}
+                >
+                  {wishlist.imageUrl ? (
+                    <img
+                      src={wishlist.imageUrl}
+                      className="w-full object-cover object-center"
+                    />
+                  ) : (
+                    <div className="flex h-full w-full items-center justify-center bg-green-100">
+                      {" "}
+                      <Scroll size={50} />{" "}
+                    </div>
+                  )}
+                </MockCard>
+              </div>
+              <ArrowRight className="animate-pulse" size={40} />
+
+              <div>
+                <p className="mb-2 text-xl font-medium "> To: </p>
+
+                <MockCard
+                  title="My Gifts"
+                  color={getBackgroundColor("pink") ?? "bg-white"}
+                >
                   <div className="flex h-full w-full items-center justify-center bg-green-100">
                     {" "}
-                    <Scroll size={50} />{" "}
+                    <Gift size={50} />{" "}
                   </div>
-                )}
-              </MockCard>
-            </div>
-            <ArrowRight className="animate-pulse" size={40} />
-
-            <div>
-              <p className="mb-2 text-xl font-medium "> To: </p>
-
-              <MockCard
-                title="My Gifts"
-                color={getBackgroundColor("pink") ?? "bg-white"}
-              >
-                <div className="flex h-full w-full items-center justify-center bg-green-100">
-                  {" "}
-                  <Gift size={50} />{" "}
-                </div>
-              </MockCard>
+                </MockCard>
+              </div>
             </div>
           </div>
+          <div className="flex w-full items-start justify-between">
+            <Button variant="secondary" onClick={() => setFrame("attribute")}>
+              Back
+            </Button>
+            <SubmitButton className="w-fit">
+              {" "}
+              {selectedUserId
+                ? "Mark item received"
+                : "Skip and mark item received"}
+            </SubmitButton>{" "}
+          </div>
         </div>
-        <motion.div
-          key="received"
-          initial={{
-            translateY: 100,
-            rotate: 0,
-          }}
-          animate={{
-            translateY: 0,
-            rotate: 9,
-          }}
-          exit={{
-            translateY: 100,
-            rotate: 0,
-          }}
-          transition={{
-            delay: animationDelaySeconds,
-          }}
-          className="absolute bottom-2 right-2"
-        >
-          <ColoredIconWrapper className="border-dashed  ">
-            <PackageOpen strokeWidth={2} size={65} color="black" />
-          </ColoredIconWrapper>
-        </motion.div>
-        <div className="mt-4">
-          <h3 className="text-lg font-medium">Who got this for you?</h3>
-          <p className="mb-4 text-balance text-sm tracking-tight">
-            Select a user from the list below to credit them for the item
-          </p>
-          <ul className="mb-6 flex max-h-[210px] flex-col gap-1 overflow-y-auto rounded-md border-2 border-black p-2">
-            {wishlistShares.map(({ users: sharedUser }) => (
-              <li key={sharedUser.id}>
-                <button
-                  type="button"
-                  onClick={() => {
-                    if (selectedUserId === sharedUser.id) {
-                      setSelectedUserId(undefined);
-                    } else {
-                      setSelectedUserId(sharedUser.id);
-                    }
-                  }}
-                  className={cn(
-                    "flex w-full flex-row items-center justify-between gap-2 rounded-md p-2 transition-all ",
-                    selectedUserId === sharedUser.id
-                      ? "bg-green-200 font-medium"
-                      : "hover:bg-blue-200",
-                  )}
-                >
-                  <div className="flex items-center gap-2">
-                    <SharedUserThumbnail>
-                      {sharedUser.image ? (
-                        <img src={sharedUser.image} />
-                      ) : (
-                        <div className="h-full w-full bg-green-300"> </div>
-                      )}
-                    </SharedUserThumbnail>
-                    <p className="max-w-[180px] overflow-ellipsis text-center text-lg leading-tight">
-                      {sharedUser.id === session.user.id
-                        ? "You"
-                        : sharedUser.name}
-                    </p>
-                  </div>
-                  {selectedUserId === sharedUser.id && <Check size={20} />}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <SubmitButton className="w-fit">
-          {" "}
-          {selectedUserId
-            ? "Mark item received"
-            : "Skip and mark item received"}
-        </SubmitButton>{" "}
+      </form>
+    );
+  }
+
+  return (
+    <div className="flex w-full flex-col ">
+      <motion.div
+        key="received"
+        initial={{
+          translateY: 100,
+          rotate: 0,
+        }}
+        animate={{
+          translateY: 0,
+          rotate: 9,
+        }}
+        exit={{
+          translateY: 100,
+          rotate: 0,
+        }}
+        transition={{
+          delay: animationDelaySeconds,
+        }}
+        className="absolute bottom-4 left-4"
+      >
+        <ColoredIconWrapper className="border-dashed  ">
+          <PackageOpen strokeWidth={2} size={50} color="black" />
+        </ColoredIconWrapper>
+      </motion.div>
+      <div className="mt-4">
+        <h3 className="text-lg font-medium">Who got this for you?</h3>
+        <p className="mb-4 text-balance text-sm tracking-tight">
+          Select a user from the list below to credit them for the item
+        </p>
+        <ul className="mb-6 flex max-h-[210px] flex-col gap-1 overflow-y-auto rounded-md border-2 border-black p-2">
+          {wishlistShares.map(({ users: sharedUser }) => (
+            <li key={sharedUser.id}>
+              <button
+                type="button"
+                onClick={() => {
+                  if (selectedUserId === sharedUser.id) {
+                    setSelectedUserId(undefined);
+                  } else {
+                    setSelectedUserId(sharedUser.id);
+                  }
+                }}
+                className={cn(
+                  "flex w-full flex-row items-center justify-between gap-2 rounded-md p-2 transition-all ",
+                  selectedUserId === sharedUser.id
+                    ? "bg-green-200 font-medium"
+                    : "hover:bg-blue-200",
+                )}
+              >
+                <div className="flex items-center gap-2">
+                  <SharedUserThumbnail>
+                    {sharedUser.image ? (
+                      <img src={sharedUser.image} />
+                    ) : (
+                      <div className="h-full w-full bg-green-300"> </div>
+                    )}
+                  </SharedUserThumbnail>
+                  <p className="max-w-[180px] overflow-ellipsis text-center text-lg leading-tight">
+                    {sharedUser.id === session.user.id
+                      ? "You"
+                      : sharedUser.name}
+                  </p>
+                </div>
+                {selectedUserId === sharedUser.id && <Check size={20} />}
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-    </form>
+      <Button
+        onClick={() => {
+          setFrame("confirm");
+        }}
+        className=" w-fit self-end "
+      >
+        {" "}
+        {selectedUserId ? "Mark item received" : "Skip and mark item received"}
+      </Button>{" "}
+    </div>
   );
 };
 
